4eeedf1344cfb79cdce37d482f9666186ead817a
Posch <Helmut.Posch@capco.com>
2018-10-09 14:51:41 +0200

    Initial commit

---
+++ src/kiwi-travelling-salesman-problem-2018.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : kiwi-travelling-salesman-problem-2018.cpp
+// Author      : Helmut Posch | Martin Zalondek | Vladimir Simcak
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Going to win!" << endl; // prints Going to win!
+	return 0;
+}
