fe4548c4c2c11014ac7a6dc004bf1692270660a6
Vladimir Simcak <vladimir.simcak@siemens.com>
2018-10-19 23:43:43 +0200

    feat: automated script functionality with output files comparison

--- test/python_folder/script_runner.py
+++ test/python_folder/script_runner.py
@@ -2,38 +2,12 @@ import os
 import sys
 import glob
 import time
+import filecmp
 import subprocess
+from typing import Optional, Any
 
 
-def trash():
-    parent_path = os.path.dirname(os.path.abspath((os.path.dirname(__file__))))
-    input_path = parent_path + '\\resources\\input'
-    output_path = parent_path + '\\resources\\output'
-
-    print(parent_path)
-    print(input_path)
-    print(output_path)
-    print(sys.path)
-    sys.path.append(input_path)
-
-    my_file = 'test0-input.txt'
-    index = 0
-
-    while not os.path.isfile(f'hello{index}.txt'):
-        print('no file')
-        index += 1
-    else:
-        print('yes file')
-
-    try:
-        print(os.path.isfile(input_path + '\\' + my_file))
-        with open(input_path + '\\' + my_file) as file:
-            file.read()
-    except FileNotFoundError:
-        print(f'No file named {my_file} found!')
-
-
-def get_input_and_output_path():
+def get_input_and_output_path() -> list:
     parent_path = os.path.dirname(os.path.abspath((os.path.dirname(__file__))))
     input_path = parent_path + '\\resources\\input'
     output_path = parent_path + '\\resources\\output'
@@ -54,36 +28,57 @@ def chdir(path: str):
     os.chdir(path)
 
 
+def compare_outputs(output_path: str, solver_output_path: str, test_number: int) -> Optional[Any]:
+    file1 = '\\'.join([output_path, f'test{test_number}-output.txt'])
+    file2 = '\\'.join([solver_output_path, f'solver{test_number}-output.txt'])
+
+    try:
+        return filecmp.cmp(file1, file2)
+    except FileNotFoundError:
+        print(f'Unable to compare files: missing output or solver_output file')
+        return None
+
+
 def run_java(java_file_path: str, test_number: int):
     java_file = extract_file_name_from_abs_path(java_file_path)
     abs_path = extract_file_path_from_abs_path(java_file_path)
 
-    start_time = time.clock()
-    time.sleep(1)
     chdir(abs_path)
-    print(f'Hi, your path: {abs_path}, test number: {test_number}')
+    start_time = time.clock()
+    time.sleep(0.5)
     # subprocess.check_call(['java', java_file, test_number], shell=True)
     end_time = time.clock()
-    print(f'time elapsed: {end_time - start_time}')
+
+    print(f'Test number: {test_number}')
+    print(f'Time elapsed: {end_time - start_time}')
 
 
 def start_testing(java_file_path: str, test_number: int):
     input_path, output_path, solver_output_path = get_input_and_output_path()
 
     chdir(input_path)
+    print('-------------------------------------------------------')
     if test_number == -1:
         print('No second argument specified, executing all tests...')
+        print('-------------------------------------------------------')
         for test_number, _ in enumerate(glob.glob('test[0-9]-input.txt')):
             run_java(java_file_path, test_number)
+            print(f'Solution match: {compare_outputs(output_path, solver_output_path, test_number)}')
+            print('-------------------------------------------------------')
 
         if test_number == -1:
             print(f'No input files with \'testX-input.txt\' format found in {os.getcwd()}')
+            print('-------------------------------------------------------')
             return
     else:
         if os.path.isfile(f'test{test_number}-input.txt'):
             run_java(java_file_path, test_number)
+            print(f'Solution match: {compare_outputs(output_path, solver_output_path, test_number)}')
+            print('-------------------------------------------------------')
+
         else:
             print(f'File with test number {test_number} not found!')
+            print('-------------------------------------------------------')
             return
 
 
